41f786f5eb6f4d3c36889110688bb323c074cc22
Naioky <wfiaux2@gmail.com>
2022-07-17 00:10:52 -0300

    forms routes
    #03 add form, routes and map template

--- course-manager/src/app/courses/course.service.ts
+++ course-manager/src/app/courses/course.service.ts
@@ -11,6 +11,16 @@ export class CourseService {
         return COURSES;
     }
 
+
+    retrieveById(id: number): Course {
+        return COURSES.find((courseItereator: Course) => courseItereator.id === id)!;
+    }
+    save (course: Course): void {
+        if (course.id){
+            const index = COURSES.findIndex((courseItereator: Course) => courseItereator.id ==course.id);
+            COURSES[index] = course;
+        }
+    }
 }
 
 
